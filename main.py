11dcfd4ffcd35244ff5f67b5f22e78554a5c46ae
Ali Raza <alirazaonsite@gmail.com>
2023-07-05 13:07:17 -0400

    main.py defined to explain projects functionality

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-import sys
+#Author: Ali Raza
+#email: ali.raza@planetofit.ca
 
 #date and time object stored in a var
 # run a time loop when a call is recieved
 # this will be the main call 
 # currently no access to src folder
 # src folder is work in progress mode to complete this api
+
+# This a concept no a complete script:
+# A java script call is recieved in a message:
+
+message = (x,y,z)
+#parse through message
+x="active port number of the caller"
+y="process id of the caller's process"
+z="numbered id of the message recieved and sent duratio"
+
+#combining 3 co-ordinates gives zelda
+message="3x+1"
+
+#3x+1 conjucture is explained mathmatics
